7ef7bdb2481de94ee7d4eaf3c0cd3a61c5133a2a
“Evan <frommere@uchicago.edu>
2022-04-27 12:02:19 -0400

    first two methods on medium

--- aa_questions/questions_database.rb
+++ aa_questions/questions_database.rb
@@ -1,61 +1,17 @@
 require 'sqlite3'
 require 'singleton'
 
-#class QuestionsDatabase < SQLite3::Database
-#    include Singleton
-#
-#    SQL_FILE = File.join(File.dirname(__FILE__), 'import_db.sql')
-#    DB_FILE = File.join(File.dirname(__FILE__), 'questions.db')
-#
-#    def initialize
-#        super('questions_database.rb')
-#        self.type_translation = true
-#        self.results_as_hash = true
-#    end
-#end
-
 class QuestionsDatabase < SQLite3::Database
-  include Singleton
+   include Singleton
 
-  SQL_FILE = File.join(File.dirname(__FILE__), 'import_db.sql')
-  DB_FILE = File.join(File.dirname(__FILE__), 'questions.db')
+   SQL_FILE = File.join(File.dirname(__FILE__), 'import_db.sql')
+   DB_FILE = File.join(File.dirname(__FILE__), 'questions.db')
 
-  def self.open
-    @database = SQLite3::Database.new(DB_FILE)
-    @database.results_as_hash = true
-    @database.type_translation = true
-  end
-
-  def self.instance
-    reset! if @database.nil?
-    @database
-  end 
-  
-  def self.reset!
-    commands = [
-      "rm '#{DB_FILE}'",
-      "cat '#{SQL_FILE}' | sqlite3 '#{DB_FILE}'"
-    ]
-    
-    commands.each {|command| `#{command}` }
-    QuestionsDatabase.open
-  end 
-
-  def self.execute(*args)
-    instance.execute(*args)
-  end
-
-  def self.get_first_row(*args)
-    instance.get_first_row(*args)
-  end
-
-  def self.get_first_value(*args)
-    instance.get_first_value(*args)
-  end
-
-  def self.last_insert_row_id
-    instance.last_insert_row_id
-  end
+   def initialize
+       super('questions.db')
+       self.type_translation = true
+       self.results_as_hash = true
+   end
 end
 
 class User
@@ -79,7 +35,7 @@ class User
             *
         FROM
             users
-        WHERE fname = ?, lname = ?
+        WHERE fname = ? AND lname = ?
         SQL
         return nil if names.empty?
         User.new(names.first)
@@ -124,7 +80,12 @@ class Question
         return nil if ids.empty?
         Question.new(ids.first)
     end
-
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end
     def author
         @author_id
     end
@@ -136,17 +97,55 @@ end
 
 class QuestionFollow
     attr_accessor :id, :question_id, :user_id
-    # def self.find_by_id(id)
-    #     question_follows = QuestionsDatabase.instance.execute(<<-SQL, id)
-    #     SELECT
-    #         *
-    #     FROM
-    #         question_follows
-    #     WHERE id = ?
-    #     SQL
-    #     return nil if question_follows.empty?
-    #     QuestionFollow.new(question_follows.first)
-    # end
+    def self.find_by_id(id)
+        question_follows = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT
+            *
+        FROM
+            question_follows
+        WHERE id = ?
+        SQL
+        return nil if question_follows.empty?
+        QuestionFollow.new(question_follows.first)
+    end
+    def self.followers_for_question_id(question_id)
+        followers_for_question_id = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+        SELECT
+            users.fname, users.id, users.lname
+        FROM
+            question_follows
+        LEFT JOIN
+            users
+        ON
+            users.id = user_id
+        WHERE question_id = ?
+        SQL
+        return nil if followers_for_question_id.empty?
+        output =[]
+        followers_for_question_id.each do |followers|
+            output << User.new(followers)
+        end
+        output
+    end
+    def self.followed_questions_for_user_id(user_id)
+        followed_questions_for_user_id = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+        SELECT
+            questions.id, questions.title, questions.body, questions.author_id
+        FROM
+            question_follows
+        LEFT JOIN
+            questions
+        ON
+            questions.id = question_id
+        WHERE user_id = ?
+        SQL
+        return nil if followed_questions_for_user_id.empty?
+        output =[]
+        followed_questions_for_user_id.each do |followed_questions|
+            output << Question.new(followed_questions)
+        end
+        output
+    end
     def initialize(options)
         @id = options['id']
         @question_id = options['question_id']
@@ -186,7 +185,7 @@ class Reply
         @user_id = options['user_id']
     end
 
-    def find_by_user_id(user_id)
+    def self.find_by_user_id(user_id)
         ids = QuestionsDatabase.instance.execute(<<-SQL, user_id)
         SELECT
             *
@@ -198,7 +197,7 @@ class Reply
         Question.new(ids.first)
     end
 
-    def find_by_question_id(question_id)
+    def self.find_by_question_id(question_id)
         ids = QuestionsDatabase.instance.execute(<<-SQL, question_id)
         SELECT
             *
