be829c75f331c3971704479b5d05310a4bf46988
Michael Chen <michael.chen.1120@gmail.com>
2022-04-27 09:33:22 -0400

    Finish import file

---
+++ aa_questions/import_db.sql
@@ -0,0 +1,64 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_follows;
+
+CREATE TABLE question_follows (
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    reply_id INTEGER,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (reply_id) REFERENCES replies(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO 
+    users(fname, lname)
+VALUES
+    ('user', '1');
+
+
+INSERT INTO 
+    questions (title, body, author_id)
+VALUES 
+    ('question 1', 'testing', 1);
